9e8124853b89b461b486dc3b92f165724fa0479b
s20straalek <s20straalek@vea.lv>
2022-03-16 10:52:36 +0200

    Edit: Person.java - created all functions without any rules

--- src/models/Person.java
+++ src/models/Person.java
@@ -5,4 +5,40 @@ public class Person {
     private String name;
     private String surname;
     private String personalCode;
+    //get funkcijas
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    } 
+    public String getPersonalCode() {
+        return personalCode;
+    }
+    //set funkcijas
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public void setPersonalCode(String personalCode) {
+        this.personalCode = personalCode;
+    }
+    //Constructor
+    Person() {
+        setName("None");
+        setSurname("None");
+        setPersonalCode("None");
+    }
+    Person(String name, String surname, String personalCode) {
+        setName(name);
+        setSurname(surname);
+        setPersonalCode(personalCode);
+    }
+    //toString funkcija
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", personalCode=" + personalCode + ", surname=" + surname + "]";
+    }
 }
